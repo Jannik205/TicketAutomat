127f853cdefa7da0ea0009001058281d7483e585
Liquidz <liquidz@liquidz.one>
2016-08-27 12:55:15 +0200

    Created Project

---
+++ TicketAutomat/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicketAutomat
+{
+    public partial class TicketAutomat : Form
+    {
+        public TicketAutomat()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
