e3bc70040e306cd7b1dc7eab132acc35fa775480
Jannik205 <jannikherrmann@web.de>
2016-08-27 13:13:52 +0200

    Anchor edited

--- TicketAutomat/Form1.Designer.cs
+++ TicketAutomat/Form1.Designer.cs
@@ -59,6 +59,7 @@
             // 
             // button1
             // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.button1.Location = new System.Drawing.Point(316, 130);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 23);
@@ -68,6 +69,7 @@
             // 
             // textBox1
             // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.textBox1.Location = new System.Drawing.Point(316, 85);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(53, 20);
@@ -75,6 +77,7 @@
             // 
             // label3
             // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(376, 91);
             this.label3.Name = "label3";
